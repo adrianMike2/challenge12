38c09b81c53e8457dddb2d761dc1f5d29b24ae47
Adrian Mike <sargentamp149@gmail.com>
2021-03-04 12:52:40 -0500

    seeds done

---
+++ seeds.sql
@@ -0,0 +1,29 @@
+INSERT INTO department (name)
+VALUES
+
+("Sales"),
+("Marketing"),
+("Enginering"),
+("Tech"),
+("Customer Service");
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+
+("Manager", 90000, 1),
+("Supervisor", 70000, 2),
+("Team Lead", 60000, 3),
+("Associate", 50000, 4),
+("Janitor", 45000, 5);
+
+INSERT INTO employee( first_name, last_name)
+VALUES
+("Robert", "Hawk"),
+("Ne", "Zha"),
+("Jim", "Perez"),
+("Joesph", "Marco"),
+("Samuel", "Haden"),
+("Patrick", "White"),
+("william", "James"),
+("miguel", "Gonezalez"),
+("Aiden", "Perez");
